14ea371f71754d486c21cb37d892fd197534d090
Shivani giri <Shivanigiri36@gmail.com>
2025-04-14 01:01:03 +0530

    adding changes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const jsonServer = require("json-server")
-const server = jsonServer.create()
-const router =jsonServer.router("db.json")
-const middlewares = jsonServer.defaults()
-const port = 3001;
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3001;
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 
-server.listen(port)
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
